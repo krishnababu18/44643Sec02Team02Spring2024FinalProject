7d5d94006763986052a5e3e12674494977866281
Manideeo4444 <manideepg200@gmail.com>
2024-03-08 02:41:20 -0600

    Gattineni Manideep added the login validation alert messages

--- DeccanVhronicle/DeccanVhronicle/ViewController.swift
+++ DeccanVhronicle/DeccanVhronicle/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginBTN: UIButton!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func LOGIN(_ sender: Any) {
+        if emailTF.text == "" {
+            showmsg("Please enter your email.")
+            return
+        }
+        if passwordTF.text == "" {
+            showmsg("Please enter your password.")
+            return
+            
+        }
+    }
+    func showmsg(_ message: String){
+        let alert = UIAlertController(title: "Message", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
