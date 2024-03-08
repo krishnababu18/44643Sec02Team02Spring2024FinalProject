394653f87f7ee0ae80087e66ab3758ead95da660
Manideeo4444 <manideepg200@gmail.com>
2024-03-08 15:47:13 -0600

    ManideepGattineni:added new Regestration page commit

---
+++ DeccanVhronicle/DeccanVhronicle/SignUpPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpPageViewController.swift
+//  DeccanVhronicle
+//
+//  Created by Manideep IOS on 3/8/24.
+//
+
+import UIKit
+
+class SignUpPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
