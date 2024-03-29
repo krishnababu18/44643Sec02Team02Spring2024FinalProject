ca64971d5cd30b78463507b0a69dd64574dcc694
premrakesh <s565102@nwmissouri.edu>
2024-03-29 17:13:16 -0500

    Prem Rakesh

--- EventsAndWeatherGuide/EventsAndWeatherGuide/LoginViewController.swift
+++ EventsAndWeatherGuide/EventsAndWeatherGuide/LoginViewController.swift
@@ -9,33 +9,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        
-    }
+            super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
+            signInButton.isEnabled = false
+            emailTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+            passwordTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        @objc func textFieldDidChange(_ textField: UITextField) {
+            guard let email = emailTextField.text, let password = passwordTextField.text else {
+                return
+            }
+            signInButton.isEnabled = isValidEmail(email) && !password.isEmpty
+        }
 
-    @IBAction func login(_ sender: Any) {
-        
-        
-        self.performSegue(withIdentifier: "loginToTabbar", sender: self)
+        func isValidEmail(_ email: String) -> Bool {
+            let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+            let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+            return emailPredicate.evaluate(with: email)
+        }
+
+        @IBAction func login(_ sender: Any) {
+            guard let email = emailTextField.text, let password = passwordTextField.text else {
+                return
+            }
+
+            if isValidEmail(email) && password == "Password" {
+                // Conditions are met, perform segue
+                performSegue(withIdentifier: "loginToTabbar", sender: self)
+            } else {
+                // Display an error message or alert indicating invalid email or password
+                print("Invalid email or password. Please try again.")
+            }
+        }
     }
-}
