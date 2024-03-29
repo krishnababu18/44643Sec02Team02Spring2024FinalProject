5c5a75e012902fb124e71e4bcaa07c4aa391140c
premrakesh <s565102@nwmissouri.edu>
2024-03-29 18:06:38 -0500

    Prem Rakesh commit-2

--- EventsAndWeatherGuide/EventsAndWeatherGuide/ForgotPasswordViewController.swift
+++ EventsAndWeatherGuide/EventsAndWeatherGuide/ForgotPasswordViewController.swift
@@ -1,33 +1,36 @@
-//
-//  ForgotPasswordViewController.swift
-//  EventsAndWeatherGuide
-//
-//  Created by Gattineni manideep on 26/03/2024.
-//
-
 import UIKit
 
 class ForgotPasswordViewController: UIViewController {
 
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var sendLinkButton: UIButton! // Connect this outlet to your "Send Link" button
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Initially disable the "Send Link" button
+        sendLinkButton.isEnabled = false
+        
+        // Add a target to the email text field to call a function when the text changes
+        emailTF.addTarget(self, action: #selector(emailTextFieldDidChange(_:)), for: .editingChanged)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Function to validate email address format and enable/disable the button accordingly
+    @objc func emailTextFieldDidChange(_ textField: UITextField) {
+        if let email = textField.text {
+            // Enable the button if the email is in proper format, disable otherwise
+            sendLinkButton.isEnabled = isValidEmail(email)
+        }
     }
-    */
+    
+    // Function to validate email address format
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
 
     @IBAction func cancel(_ sender: Any) {
-        
         self.dismiss(animated: true)
     }
 }
