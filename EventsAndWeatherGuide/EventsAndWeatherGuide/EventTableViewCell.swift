8eeb88c30a8d73e14b1deeac340e8e880646078a
krishna babu gali <krishnababugali18@gmail.com>
2024-03-29 21:28:15 -0500

    Gali Krishna Babu- Added Event Table View Controller

---
+++ EventsAndWeatherGuide/EventsAndWeatherGuide/EventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventTableViewCell.swift
+//  EventsAndWeatherGuide
+//
+//  Created by Gattineni manideep on 30/03/2024.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
