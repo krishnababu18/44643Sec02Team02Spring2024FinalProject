ee3e9e7a583b0fe1bd14819033c984d4e4a57b3c
krishna babu gali <krishnababugali18@gmail.com>
2024-04-12 15:21:10 -0500

    Gali krishna babu added profile view controller

---
+++ EventsAndWeatherGuide/EventsAndWeatherGuide/ProfileViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ProfileViewController.swift
+//  EventsAndWeatherGuide
+//
+//  Created by Gattineni manideep on 02/04/2024.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLbl.text = Auth.auth().currentUser?.displayName ?? ""
+        emailLbl.text = Auth.auth().currentUser?.email ?? ""
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logout(_ sender: Any) {
+        
+        UserDefaults.standard.removeObject(forKey: "type")
+        UserDefaults.standard.synchronize()
+        
+        do {
+            
+            try Auth.auth().signOut()
+        } catch {}
+        
+        
+        self.performSegue(withIdentifier: "profileToLogin", sender: self)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
