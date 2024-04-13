bba410ebe5157b1e208d6f9e752161ca95d1e3f7
krishna babu gali <krishnababugali18@gmail.com>
2024-04-12 20:53:32 -0500

    gali krishna babu-added table view

---
+++ EventsAndWeatherGuide/EventsAndWeatherGuide/CustomTableView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableView.swift
+//  EventsAndWeatherGuide
+//
+//  Created by Krishna Babu Gali on 4/12/24.
+//
+
+import UIKit
+
+class CustomTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
